7f8a80b0db54393054ff2acffd0f8bdea560572a
Teguh Kurniawan <teguhkrniawan@gmail.com>
2022-10-03 19:39:49 +0700

    add - auth feature done

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,10 +1,14 @@
 import express from "express"
 import {
-    login
+    login,
+    logout,
+    me
 } from '../controllers/AuthController.js'
 
 const router = express.Router();
 
+router.get('/me', me);
 router.post('/login', login);
+router.delete('/logout', logout)
 
 export default router
